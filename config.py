6e763e1bb2cfc7093df0f5e1dada5a2b4ef87f91
tomoyuki@sakura <tomoyuki@www1269ue.sakura.ne.jp>
2013-11-24 00:13:24 +0900

    add comment

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
+# DBホスト名
 DB_HOST = "localhost"
+# DB名
 DB_NAME = "nyusatsu_check"
+# DBユーザ名
 DB_USER = "nyusatsu_check"
+# DBパスワード
 DB_PASSWORD = "nyusatsu_check"
 
+# 情報取得サイトURL
 SITE_URL = u'http://wave.pref.wakayama.lg.jp/ekimu/'
 
+# ログファイルディレクトリ
 LOG_FILE_DIR_PATH = "./log/"
