5fe456620b0d23dc28412b46ddf5b2a51adec5e6
tomoyuki <hzd02765@nifty.com>
2015-02-27 16:40:29 +0900

    接続先を変更、デバッグ文をコメントアウト

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ DB_USER = "nyusatsu_check"
 DB_PASSWORD = "nyusatsu_check"
 
 # 情報取得サイトURL
-SITE_URL = u'http://wave.pref.wakayama.lg.jp/ekimu/'
+SITE_URL = u'http://wave.pref.wakayama.lg.jp/ekimu2/'
 # サイト名
-SITE_NAME = "ekimu"
+SITE_NAME = "ekimu2"
 
 # ログファイルディレクトリ名
 LOG_FILE_DIR_PATH = "log/"
